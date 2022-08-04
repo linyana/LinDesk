537e341065040fd06bdbabc922bdc1bfc5b43bf5
lin <1439713475@qq.com>
2022-08-04 13:40:41 +0800

    更新搜索功能

---
+++ src/components/Nav.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="Nav">
+        <div class="menu">
+            <div class="logo"></div>
+            <div class="menu_line">
+                <img src="../assets/images/nav/1.png" alt />
+                <div class="menu_line_text">搜索</div>
+            </div>
+            <div class="menu_line">
+                <img src="../assets/images/nav/4.png" alt />
+                <div class="menu_line_text">音乐</div>
+            </div>
+            <div class="menu_line">
+                <img src="../assets/images/nav/3.png" alt />
+                <div class="menu_line_text">视频</div>
+            </div>
+            <div class="menu_line">
+                <img src="../assets/images/nav/2.png" alt />
+                <div class="menu_line_text">友链</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#Nav {
+    width: 100%;
+    height: 100%;
+    border-top-left-radius: 30px;
+    border-bottom-left-radius: 30px;
+    background-color: #2d3647;
+}
+
+.logo {
+    margin-left: 15%;
+    width: 70%;
+    height: 100px;
+    background-image: url(../assets/images/logo.png);
+    background-size: 100% 100%;
+    margin-bottom: 30px;
+}
+
+.menu {
+    padding-top: 30px;
+}
+
+.menu_line {
+    cursor: pointer;
+    position: relative;
+    height: 60px;
+    margin: 10px 20px;
+    border-radius: 20px;
+}
+
+.menu_line:hover {
+    background-color: #292e38;
+}
+
+img {
+    position: absolute;
+    left: 40px;
+    top: 17px;
+    width: 26px;
+}
+
+.menu_line_text {
+    color: rgb(184, 184, 184, 0.5);
+    position: absolute;
+    left: 90px;
+    line-height: 60px;
+    font-size: 25px;
+}
+</style>
---
+++ src/pages/Search.vue
@@ -0,0 +1,279 @@
+<template>
+    <div id="Search" @click="CloseCard">
+        <br />
+        <div class="input">
+            <input type="text" @keydown.enter="Search()" v-model="search_content" />
+        </div>
+        <div class="select_boxes">
+            <div class="select_box" :style="height1">
+                <div class="select_box_input" @click.stop="OpenCard('height1','open')">
+                    <div class="results">
+                        <img :src="img1" class="select_box_card_img" />
+                        <div class="select_box_card_text">{{text1}}</div>
+                    </div>
+                </div>
+                <div class="select_box_cards">
+                    <div
+                        class="select_box_card"
+                        v-for="item in options1"
+                        :key="item.value"
+                        @click="GetValue1(item)"
+                    >
+                        <img :src="item.img" class="select_box_card_img" />
+                        <div class="select_box_card_text">{{item.label}}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="select_box" :style="height2">
+                <div class="select_box_input" @click.stop="OpenCard('height2','open')">
+                    <div class="results">
+                        <img :src="img2" class="select_box_card_img" />
+                        <div class="select_box_card_text">{{text2}}</div>
+                    </div>
+                </div>
+                <div class="select_box_cards">
+                    <div
+                        class="select_box_card"
+                        v-for="item in options2"
+                        :key="item.value"
+                        @click="GetValue2(item)"
+                    >
+                        <img :src="item.img" class="select_box_card_img" />
+                        <div class="select_box_card_text">{{item.label}}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="select_box" :style="height3">
+                <div class="select_box_input" @click.stop="OpenCard('height3','open')">
+                    <div class="results">
+                        <div class="select_box_card_text">内容待定</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    el: "#Search",
+    data() {
+        return {
+            target: "",
+            search_content: "",
+            value1: "https://www.baidu.com/s?wd=",
+            value2: "chrome",
+            value3: "https://www.baidu.com/s?wd=",
+            height1: "height: 70px;",
+            height2: "height: 70px;",
+            height3: "height: 70px;",
+            img1: "/src/assets/images/baidu.png",
+            img2: "/src/assets/images/chrome.png",
+            img3: "/src/assets/images/baidu.png",
+            text1: "百度",
+            text2: "chrome",
+            text3: "百度",
+            music: "",
+            options1: [
+                {
+                    label: "百度",
+                    value: "https://www.baidu.com/s?wd=",
+                    img: "/src/assets/images/baidu.png",
+                },
+                {
+                    label: "bilibili",
+                    value: "https://search.bilibili.com/all?keyword=",
+                    img: "/src/assets/images/bilibili.png",
+                },
+                {
+                    label: "github",
+                    value: "https://github.com/search?q=",
+                    img: "/src/assets/images/github.png",
+                },
+            ],
+            options2: [
+                {
+                    value: "edge",
+                    label: "edge",
+                    img: "/src/assets/images/edge.png",
+                },
+                {
+                    value: "chrome",
+                    label: "chrome",
+                    img: "/src/assets/images/chrome.png",
+                },
+                {
+                    value: "firefox",
+                    label: "firefox",
+                    img: "/src/assets/images/firefox.png",
+                },
+            ],
+            options3: [
+                {
+                    value: "https://www.baidu.com/s?wd=",
+                    label: "百度",
+                },
+                {
+                    value: "https://search.bilibili.com/all?keyword=",
+                    label: "bilibili",
+                },
+                {
+                    value: "https://github.com/search?q=",
+                    label: "github",
+                },
+            ],
+        };
+    },
+    watch: {
+        search_content: {
+            handler() {
+                this.target = this.value1 + this.search_content;
+            },
+        },
+    },
+    methods: {
+        Search() {
+            window.open(this.target);
+            this.search_content = "";
+        },
+        CloseCard() {
+            this.height1 = "height: 70px;";
+            this.height2 = "height: 70px;";
+            this.height3 = "height: 70px;";
+        },
+        // 拉开下拉框
+        OpenCard(index, flag) {
+            if (index === "height1") {
+                this.height1 = "height: 220px";
+            }
+            if (index === "height2") {
+                this.height2 = "height: 220px";
+            }
+            if (index === "height3") {
+                this.height3 = "height: 220px";
+            }
+        },
+        // select框一选中
+        GetValue1(item) {
+            this.value1 = item.value;
+            this.img1 = item.img;
+            this.text1 = item.label;
+        },
+        // select框二选中
+        GetValue2(item) {
+            this.value2 = item.value;
+            this.img2 = item.img;
+            this.text2 = item.label;
+        },
+    },
+};
+</script>
+
+<style scoped>
+#Search {
+    width: 100%;
+    height: 100%;
+    margin: auto;
+    text-align: center;
+}
+
+.input {
+    width: 600px;
+    height: 40px;
+    margin: 100px auto;
+    margin-top: 150px;
+}
+
+input {
+    width: 100%;
+    height: 100%;
+    border-radius: 15px;
+    outline: none;
+    border: none;
+    text-indent: 1em;
+    background-color: rgba(218, 222, 230, 0.5);
+    color: rgb(184, 184, 184);
+    font-size: 19px;
+}
+
+select {
+    margin: 20px;
+    outline: none;
+}
+
+.search_background {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 40%;
+    background-image: url(../assets/images/search_background.png);
+    background-size: 100% 100%;
+}
+
+.select_boxes {
+    width: 60%;
+    margin: 100px auto;
+    justify-content: space-between;
+    display: flex;
+}
+
+.select_box {
+    border-radius: 20px;
+    width: 200px;
+    height: 220px;
+    background-color: #343943;
+    transition: all 0.8s;
+    overflow: hidden;
+}
+
+.select_box_input {
+    width: 100%;
+    height: 40px;
+    margin-top: 15px;
+}
+
+.select_box_cards {
+    margin-top: 10px;
+}
+
+.select_box_card {
+    cursor: pointer;
+    position: relative;
+    width: 80%;
+    margin-left: 10%;
+    height: 40px;
+    border-radius: 10px;
+    margin-top: 10px;
+}
+
+.select_box_card:hover {
+    background-color: #2b3039;
+}
+
+.select_box_card_img {
+    position: absolute;
+    top: 10px;
+    left: 20px;
+    width: 20px;
+}
+
+.select_box_card_text {
+    line-height: 40px;
+    color: rgb(184, 184, 184);
+}
+
+.results {
+    position: relative;
+    cursor: pointer;
+    display: inline-block;
+    width: 80%;
+    height: 100%;
+    border-radius: 15px;
+    outline: none;
+    border: none;
+    background-color: rgba(218, 222, 230, 0.5);
+    color: rgb(184, 184, 184);
+    font-size: 16px;
+    background-color: #2b3039;
+}
+</style>
