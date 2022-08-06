6090a625aee2c2802ceac21bc12ac13228dec814
lin <1439713475@qq.com>
2022-08-06 13:33:49 +0800

    更新了搜索页

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,85 @@
 <template>
-    <div id="App" :class="classes.theme">
-        <button @click="ChangeTheme">切换主题</button>
-        {{theme.backgroundColor}}
+    <div id="App"></div>
+    <div id="Pages" :class="classes.theme">
+        <Search />
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { reactive, ref } from "vue";
+import Search from "./pages/Search.vue";
 
-export default {
-    setup() {
-        // 主题切换
-        interface Theme {
-            color: string;
-            backgroundColor: string;
-        }
+// 主题切换
+interface Theme {
+    color: string;
+    backgroundColor: string;
+    floatBackgroundColor: string;
+}
 
-        class Dark implements Theme {
-            public color = "white";
-            public backgroundColor = "black";
-        }
+// 暗色主题
+class Dark implements Theme {
+    public color = "rbg(0,0,0,0.8)";
+    public backgroundColor = "black";
+    public floatBackgroundColor = "black";
+}
 
-        class Light implements Theme {
-            public color = "white";
-            public backgroundColor = "green";
-        }
+// 亮色主题
+class Light implements Theme {
+    public color = "rbg(0,0,0,0.3)";
+    public backgroundColor = "green";
+    public floatBackgroundColor = "rgba(255,255,255,0.5)";
+}
 
-        const dark = new Dark();
-        const light = new Light();
+// 实例化对象
+const dark = new Dark();
+const light = new Light();
 
-        let theme = ref(dark);
+// 创建theme来接收当前主题并应用
+let theme: any;
+if (localStorage.getItem("theme") === "dark") {
+    theme = ref(dark);
+} else {
+    theme = ref(light);
+}
 
-        // 改变主题
-        function ChangeTheme() {
-            if (localStorage.getItem("theme") === "light") {
-                localStorage.setItem("theme", "dark");
-                theme.value = dark;
-            } else {
-                localStorage.setItem("theme", "light");
-                theme.value = light;
-            }
-        }
-
-        return {
-            dark,
-            light,
-            theme,
-            ChangeTheme,
-        };
-    },
-};
+// 切换主题
+function ChangeTheme() {
+    if (localStorage.getItem("theme") === "light") {
+        localStorage.setItem("theme", "dark");
+        theme.value = dark;
+    } else {
+        localStorage.setItem("theme", "light");
+        theme.value = light;
+    }
+}
 </script>
 
 
-<style scoped>
+<style>
 #App {
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 0;
     width: 100%;
     height: 100%;
+    background-image: url(https://s3.bmp.ovh/imgs/2022/03/87bbaff8e7637ddb.jpg);
+    background-size: 100% 100%;
+}
+
+#Pages {
+    position: absolute;
+    top: 5%;
+    left: 100px;
+    width: calc(100% - 200px);
+    height: 90%;
 }
 </style>
 
 <style module="classes">
 .theme {
-    transition: all 0.5s;
-    background-color: v-bind("theme.backgroundColor");
-    color: v-bind("theme.color");
+    --background-color: v-bind("theme.backgroundColor");
+    --color: v-bind("theme.color");
+    --float-backgroundColor: v-bind("theme.floatBackgroundColor")
 }
-
 </style>
---
+++ src/pages/Search.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="Search">
+        <div class="search">
+            <input type="text" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.search {
+    margin: auto;
+    margin-top: 300px;
+    width: 50%;
+    max-width: 600px;
+    height: 30px;
+    background-color: var(--float-backgroundColor);
+    box-shadow: 3px 3px 10px #00000050;
+    backdrop-filter: blur(20px);
+    border-radius: 10px;
+    padding: 10px;
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 20px;
+    transition: 0.25s;
+    z-index: 5;
+}
+
+.search input {
+    width: 100%;
+    border: none;
+    background-color: transparent;
+    border-radius: 6px;
+    height: 30px;
+    font-size: 19px;
+    font-weight: 700;
+    outline: 0px;
+    color: var(--color);
+    line-height: 30px;
+    text-align: center;
+}
+</style>
