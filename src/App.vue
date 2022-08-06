4cd5956fc3e690c380013152082bfa411dd3cbf0
lin <1439713475@qq.com>
2022-08-06 16:23:42 +0800

    优化了setup

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+
 <template>
     <div id="App"></div>
     <div id="Pages" :class="classes.theme">
@@ -5,53 +6,64 @@
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { reactive, ref } from "vue";
 import Search from "./pages/Search.vue";
 
-// 主题切换
-interface Theme {
-    color: string;
-    backgroundColor: string;
-    floatBackgroundColor: string;
-}
+export default {
+    components: {
+        Search,
+    },
+    setup() {
+        // 主题切换
+        interface Theme {
+            color: string;
+            backgroundColor: string;
+            floatBackgroundColor: string;
+        }
 
-// 暗色主题
-class Dark implements Theme {
-    public color = "rbg(0,0,0,0.8)";
-    public backgroundColor = "black";
-    public floatBackgroundColor = "black";
-}
+        // 暗色主题
+        class Dark implements Theme {
+            public color = "rbg(0,0,0,0.8)";
+            public backgroundColor = "black";
+            public floatBackgroundColor = "black";
+        }
 
-// 亮色主题
-class Light implements Theme {
-    public color = "rbg(0,0,0,0.3)";
-    public backgroundColor = "green";
-    public floatBackgroundColor = "rgba(255,255,255,0.5)";
-}
+        // 亮色主题
+        class Light implements Theme {
+            public color = "rbg(0,0,0,0.3)";
+            public backgroundColor = "green";
+            public floatBackgroundColor = "rgba(255,255,255,0.5)";
+        }
 
-// 实例化对象
-const dark = new Dark();
-const light = new Light();
+        // 实例化对象
+        const dark = new Dark();
+        const light = new Light();
 
-// 创建theme来接收当前主题并应用
-let theme: any;
-if (localStorage.getItem("theme") === "dark") {
-    theme = ref(dark);
-} else {
-    theme = ref(light);
-}
+        // 创建theme来接收当前主题并应用
+        let theme: any;
+        if (localStorage.getItem("theme") === "dark") {
+            theme = ref(dark);
+        } else {
+            theme = ref(light);
+        }
 
-// 切换主题
-function ChangeTheme() {
-    if (localStorage.getItem("theme") === "light") {
-        localStorage.setItem("theme", "dark");
-        theme.value = dark;
-    } else {
-        localStorage.setItem("theme", "light");
-        theme.value = light;
-    }
-}
+        // 切换主题
+        function ChangeTheme() {
+            if (localStorage.getItem("theme") === "light") {
+                localStorage.setItem("theme", "dark");
+                theme.value = dark;
+            } else {
+                localStorage.setItem("theme", "light");
+                theme.value = light;
+            }
+        }
+        return {
+            theme,
+            ChangeTheme
+        }
+    },
+};
 </script>
 
 
@@ -80,6 +92,6 @@ function ChangeTheme() {
 .theme {
     --background-color: v-bind("theme.backgroundColor");
     --color: v-bind("theme.color");
-    --float-backgroundColor: v-bind("theme.floatBackgroundColor")
+    --float-backgroundColor: v-bind("theme.floatBackgroundColor");
 }
 </style>
