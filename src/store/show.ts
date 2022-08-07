4968808050526c4ad4456b60de9fb2492bfd829f
lin <1439713475@qq.com>
2022-08-07 21:35:14 +0800

    pinia本地存储

--- src/store/show.ts
+++ src/store/show.ts
@@ -14,6 +14,12 @@ export const showStore = defineStore('rightNav', {
         }
     },
     persist: {
-        enabled: true
+        enabled: true,
+        strategies: [
+            {
+                key: 'isShow',
+                storage: localStorage,
+            }
+        ]
     }
 })
