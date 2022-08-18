178a7c5f8f9742f50386c324bfad55cb7470ad2d
lin <1439713475@qq.com>
2022-08-18 21:46:45 +0800

    初始化天气功能

--- src/pages/Weather.vue
+++ src/pages/Weather.vue
@@ -5,32 +5,9 @@
 <script setup lang="ts">
 import { onMounted,ref } from "vue";
 
-let city: string = ref();
+let city = ref('');
 
-// 获取当前城市
-const GetCity = ()=>{
-    AMap.plugin("AMap.Geolocation", () => {
-        var geolocation = new AMap.Geolocation({
-            // 是否使用高精度定位，默认：true
-            enableHighAccuracy: true,
-        });
-        geolocation.getCityInfo((status, result) => {
-            //只能获取当前用户所在城市和城市的经纬度
-            if (status == "complete") {
-                city.value = result.city;
-                console.log(city.value)
-            }
-        });
-        geolocation.getCurrentPosition((status, result) => {
-            //获取用户当前的精确位置
-            if (status == "complete") {
-                city.value = result.city;
-            }
-        });
-    });
-}
 
-GetCity()
 
 
 
