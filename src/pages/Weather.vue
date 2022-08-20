8abf90dd8ecfe77f5d039b97ebd07771e8637587
lin <1439713475@qq.com>
2022-08-20 10:23:03 +0800

    更新了city的输入

--- src/pages/Weather.vue
+++ src/pages/Weather.vue
@@ -1,22 +1,59 @@
 <template>
     <div id="weather">
-        <div class="city_card">
-            <div class="now_city">当前城市 : {{city}}</div>
-            <div class="input"></div>
+        <div class="city_card" :style="isWriteStyle">
+            <div class="now_city" v-if="!isWrite" @click="ShowInput">当前城市 : {{city}}</div>
+            <input
+                v-if="isWrite"
+                v-model="cityInput"
+                id="change_city_input"
+                ref="refInput"
+                @keydown.enter="EnterCityInput"
+            />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, nextTick } from "vue";
 import { cityStore } from "../store/city";
 
+// pinia 记录城市
+// store.city:城市
 const store = cityStore();
 
-let city = ref(store.city);
-if (["", " ", null].includes(city.value)) {
+let city: string = ref(store.city);
+if (["", " ", null].includes(store.city)) {
     city.value = store.city = "成都";
 }
+
+
+// 输入框
+const cityInput: string = ref(store.city);
+let isWrite: boolean = ref(false);
+
+let isWriteStyle: string = ref(`width: ${100 + 20 * city.value.length}px;`);
+
+const ShowInput = () => {
+    isWrite.value = true;
+    isWriteStyle.value = "width: 400px;";
+    getFocus();
+};
+
+const EnterCityInput = () => {
+    isWrite.value = false;
+    store.city = cityInput.value;
+    city.value = store.city;
+    isWriteStyle.value = `width: ${100 + 20 * city.value.length}px;`;
+};
+
+// 焦点
+const refInput = ref();
+
+const getFocus = async () => {
+    nextTick(() => {
+        refInput.value.focus();
+    });
+};
 </script>
 
 <style scoped>
@@ -36,16 +73,28 @@ if (["", " ", null].includes(city.value)) {
     box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);
     padding: 10px;
     border-radius: 15px;
+    transition: all 0.8s;
 }
 
 .now_city {
-    width: 170px;
-    height: 40px;
+    height: 32px;
     text-align: center;
-    line-height: 40px;
+    line-height: 32px;
     font-weight: 700;
     color: var(--color);
     letter-spacing: 0.5px;
-    font-size: 20px;
+    font-size: 19px;
+}
+
+input#change_city_input {
+    width: 100%;
+    outline: none;
+    border: none;
+    text-align: center;
+    font-size: 19px;
+    color: var(--color);
+    letter-spacing: 1px;
+    font-weight: 700;
+    background-color: transparent;
 }
 </style>
