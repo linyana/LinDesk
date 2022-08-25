b71226361388dc6c6c0ac4d0760893596a2964cf
lin <1439713475@qq.com>
2022-08-25 12:57:32 +0800

    增加了设置

--- src/components/RightNav.vue
+++ src/components/RightNav.vue
@@ -3,17 +3,34 @@
         <div class="circle" @click="ChangeShow">
             <img :src="getImageUrl(img1)" />
         </div>
+        <transition name="rightNav" v-show="store.isShow">
+            <div>
+                <div class="circle" @click="open">
+                    <img :src="getImageUrl(img2)" />
+                </div>
+            </div>
+        </transition>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ElMessage } from "element-plus";
 import { showStore } from "../store/show";
+
+// 功能未开放提示
+const open = () => {
+    ElMessage({
+        showClose: true,
+        message: "加急开发中",
+    });
+};
 
 // pinia
 const store = showStore();
 
 // 图片
 const img1 = "1.png";
+const img2 = "2.png";
 
 function getImageUrl(name: string): string {
     return new URL(`../assets/images/rightNav/${name}`, import.meta.url).href;
@@ -57,5 +74,25 @@ img {
     margin-top: 10px;
     width: 30px;
     height: 30px;
+}
+
+.rightNav-enter-from,
+.rightNav-leave-to {
+    opacity: 0;
+    transform: translateX(20px);
+}
+
+.rightNav-enter-to,
+.rightNav-leave-from {
+    opacity: 1;
+    transform: translateX(0);
+}
+
+.rightNav-enter-active {
+    transition: all 0.8s ease;
+}
+
+.rightNav-leave-active {
+    transition: all 0.8s ease;
 }
 </style>
