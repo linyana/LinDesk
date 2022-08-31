65da6d1da2952ae476b36d1a2ec63595bdd5c8df
lin <1439713475@qq.com>
2022-08-31 18:48:18 +0800

    test

---
+++ src/components/setting.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="setting"></div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+#setting {
+    margin: 50% auto;
+    transform: translateY(-50%);
+    width: 200px;
+    height: 200px;
+    background-color: #fff;
+}
+</style>
